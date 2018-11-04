abe0b9d5ff6f0d4eecc0ccf9b62a539563c37b7a
Krzysztof Radomski <krzysztofradomski@krs.local>
2018-11-04 14:20:47 +0100

    feature: moved back to es5

--- dictionaries/index.js
+++ dictionaries/index.js
@@ -1,2 +1,23 @@
-export { nounsDictionary } from './nouns.js';
-export { adjectivesDictionary } from './adjectives.js';
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _nouns = require('./nouns.js');
+
+Object.defineProperty(exports, 'nounsDictionary', {
+  enumerable: true,
+  get: function get() {
+    return _nouns.nounsDictionary;
+  }
+});
+
+var _adjectives = require('./adjectives.js');
+
+Object.defineProperty(exports, 'adjectivesDictionary', {
+  enumerable: true,
+  get: function get() {
+    return _adjectives.adjectivesDictionary;
+  }
+});
